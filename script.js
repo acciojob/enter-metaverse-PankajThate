fc37674e8787a58ae5593cf1fcc83082e43ef857
PankajThate <124923727+PankajThate@users.noreply.github.com>
2023-06-25 23:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,12 +9,12 @@
 
 var para = document.getElementById("status");
 var button =  document.getElementById("enterBtn");
-//var h1 = document.getElementById("display");
+
 button.addEventListener('click', ()=>{
-	setTimeout(()=>{
+	
 	let h1 = document.createElement("h1");
 	h1.textContent = "Entered Metaverse";
 	para.parentNode.replaceChild(h1,para);
- },4000);
+ 
 	
 });
